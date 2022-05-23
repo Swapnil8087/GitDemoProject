efe05e4c31b62c0bc291749a6cf2af2a4a067820
Swapnil Nitnavare <swapnilnitnavare7@gmail.com>
2022-05-23 13:24:55 +0530

    Second Commit

--- Sum.java
+++ Sum.java
@@ -4,6 +4,6 @@ class Sum{
     public static void main(String[] args) {
         int a=5;
         int b=6;
-        System.out.println(a+b);
+        System.out.println("The Sum is: " + (a+b));
     }
 }
